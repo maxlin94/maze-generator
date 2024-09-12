40009212e05b7ac38a6aa44e02320dcd18c55e8e
Max Lindström <max941@live.se>
2024-09-12 14:24:02 +0200

    Removed unused method and instance variable

--- Node.js
+++ Node.js
@@ -12,7 +12,6 @@ export default class Node {
         this.size = size;
         this.ctx = ctx;
         this.parent = null;
-        this.borderColor = borderColor;
         this.wallThickness = 0.5;
         this.walls = {
             left: true,
@@ -20,48 +19,6 @@ export default class Node {
             up: true,
             down: true,
         };
-    }
-
-    paint() {
-        const keys = Object.keys(this.walls);
-        keys.forEach((dir) => {
-            if(this.walls[dir]) {
-                this.paintWall(dir);
-            } else {
-                this.drawLine(dir, this.borderColor);
-            }
-        });
-    }
-
-    paintWall(dir, color) {
-        this.ctx.strokeStyle = color;
-        this.ctx.lineWidth = this.wallThickness;
-        const offset = this.wallThickness / 2 * -1; // Fixes jagged corners
-        this.ctx.beginPath();
-        switch (dir) {
-            case "right":
-                this.ctx.moveTo(this.x + 1, this.y + offset);
-                this.ctx.lineTo(this.x + 1, this.y + 1 - offset);
-                this.ctx.stroke();
-                break;
-            case "left":
-                this.ctx.moveTo(this.x, this.y + offset);
-                this.ctx.lineTo(this.x, this.y + 1 - offset);
-                this.ctx.stroke();
-                break;
-            case "up":
-                this.ctx.moveTo(this.x + offset, this.y);
-                this.ctx.lineTo(this.x + 1 - offset, this.y);
-                this.ctx.stroke();
-                break;
-            case "down":
-                this.ctx.moveTo(this.x + offset, this.y + 1);
-                this.ctx.lineTo(this.x + 1 - offset, this.y + 1);
-                this.ctx.stroke();
-                break;
-            default:
-                break;
-        }
     }
 
     drawLine(dir, color) {
